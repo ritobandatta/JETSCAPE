9278de62057de2cecab0bc777ebff0a25b97581c
Ritoban Datta <hl9735@wayne.edu>
2024-09-23 13:03:50 -0400

    Updated Matter
    The Matter.cc file is updated to introduce the new Elastic Collision class, SplittingFunction and SudakovFunction namespace.

--- src/jet/Matter.h
+++ src/jet/Matter.h
@@ -13,13 +13,16 @@
  * See COPYING for details.
  ******************************************************************************/
 
-#ifndef MATTER_H
-#define MATTER_H
+#ifndef Matter_H
+#define Matter_H
 
 #include "JetEnergyLossModule.h"
 #include "Pythia8/Pythia.h"
+#include "ElasticCollision.h"
+#include "ElasticCollision.h"
 
 using namespace Jetscape;
+
 
 class Matter : public JetEnergyLossModule<
                    Matter> //, public std::enable_shared_from_this<Matter>
@@ -37,44 +40,32 @@ public:
   void Dump_pIn_info(int i, vector<Parton> &pIn);
 
   double generate_L(double form_time);
-  double sudakov_Pgg(double g0, double g1, double loc_c, double E);
-  double sud_val_GG(double h0, double h1, double h2, double loc_d, double E1);
-  double sud_z_GG(double cg, double cg1, double loc_e, double l_fac, double E2);
-  double P_z_gg_int(double cg, double cg1, double loc_e, double cg3,
-                    double l_fac, double E2);
-  double sudakov_Pqg(double g0, double g1, double loc_c, double E);
-  double sud_val_QG(double h0, double h1, double h2, double loc_d, double E1);
-  double sud_z_QG(double cg, double cg1, double loc_e, double l_fac, double E2);
-  double P_z_qg_int(double cg, double cg1, double loc_e, double cg3,
-                    double l_fac, double E2);
-  double sudakov_Pqg_w_M(double M, double g0, double g1, double loc_c,
-                         double E);
-  double sud_val_QG_w_M(double M, double h0, double h1, double h2, double loc_d,
-                        double E1);
-  double sud_z_QG_w_M(double M, double cg, double cg1, double loc_e,
-                      double l_fac, double E2);
-  double P_z_qg_int_w_M(double M, double cg, double cg1, double loc_e,
-                        double cg3, double l_fac, double E2);
-  double sudakov_Pqq(double q0, double q1, double loc_c, double E);
+  //double sudakov_Pgg(double g0, double g1, double loc_c, double E);
+  //double sud_val_GG(double h0, double h1, double h2, double loc_d, double E1);
+  //double sud_z_GG(double cg, double cg1, double loc_e, double l_fac, double E2);
+  //double P_z_gg_int(double cg, double cg1, double loc_e, double cg3,double l_fac, double E2);
+  //double sudakov_Pqg(double g0, double g1, double loc_c, double E);
+  //double sud_val_QG(double h0, double h1, double h2, double loc_d, double E1);
+  //double sud_z_QG(double cg, double cg1, double loc_e, double l_fac, double E2);
+  //double P_z_qg_int(double cg, double cg1, double loc_e, double cg3,double l_fac, double E2);
+  //double sudakov_Pqg_w_M(double M, double g0, double g1, double loc_c,double E);
+  //double sud_val_QG_w_M(double M, double h0, double h1, double h2, double loc_d, double E1);
+  //double sud_z_QG_w_M(double M, double cg, double cg1, double loc_e,double l_fac, double E2);
+  //double P_z_qg_int_w_M(double M, double cg, double cg1, double loc_e, double cg3, double l_fac, double E2);
+  //double sudakov_Pqq(double q0, double q1, double loc_c, double E);
 
-  double sud_val_QQ(double h0, double h1, double h2, double loc_d, double E1);
-  double sud_z_QQ(double cg, double cg1, double loc_e, double l_fac, double E2);
-  double P_z_qq_int(double cg, double cg1, double loc_e, double cg3,
-                    double l_fac, double E2);
-  double P_z_qp_int(double cg, double cg1, double loc_e, double cg3,
-                    double l_fac, double E2);
-  double sud_z_QP(double cg, double cg1, double loc_e, double l_fac, double E2);
-  double sud_val_QP(double h0, double h1, double h2, double loc_d, double E1);
-  double sudakov_Pqp(double g0, double g1, double loc_c, double E);
+  //double sud_val_QQ(double h0, double h1, double h2, double loc_d, double E1);
+  //double sud_z_QQ(double cg, double cg1, double loc_e, double l_fac, double E2);
+  //double P_z_qq_int(double cg, double cg1, double loc_e, double cg3,double l_fac, double E2);
+  //double P_z_qp_int(double cg, double cg1, double loc_e, double cg3, double l_fac, double E2);
+  //double sud_z_QP(double cg, double cg1, double loc_e, double l_fac, double E2);
+  //double sud_val_QP(double h0, double h1, double h2, double loc_d, double E1);
+  //double sudakov_Pqp(double g0, double g1, double loc_c, double E);
 
-  double sudakov_Pqq_w_M_vac_only(double M, double q0, double q1, double loc_c,
-                                  double E);
-  double sud_val_QQ_w_M_vac_only(double M, double h0, double h1, double h2,
-                                 double loc_d, double E1);
-  double sud_z_QQ_w_M_vac_only(double M, double cg, double cg1, double loc_e,
-                               double l_fac, double E2);
-  double P_z_qq_int_w_M_vac_only(double M, double cg, double cg1, double loc_e,
-                                 double cg3, double l_fac, double E2);
+  //double sudakov_Pqq_w_M_vac_only(double M, double q0, double q1, double loc_c,double E);
+  //double sud_val_QQ_w_M_vac_only(double M, double h0, double h1, double h2, double loc_d, double E1);
+  //double sud_z_QQ_w_M_vac_only(double M, double cg, double cg1, double loc_e,double l_fac, double E2);
+  //double P_z_qq_int_w_M_vac_only(double M, double cg, double cg1, double loc_e,double cg3, double l_fac, double E2);
 
   //  void shower_vac( int line, int pid, double nu_in, double t0_in, double t_in, double kx, double ky, double loc, bool is_lead);
   double generate_vac_t(int p_id, double nu, double t0, double t, double loc_a,
@@ -103,8 +94,7 @@ public:
   double fncQhat(double zeta);
   double fncAvrQhat(double zeta, double tau);
 
-  bool matter_on, in_vac, brick_med, recoil_on, broadening_on,
-      initial_virtuality_pT;
+  bool matter_on, in_vac, brick_med, recoil_on, broadening_on;
   double hydro_Tc, qhat0, alphas, brick_length, vir_factor;
   double initR0, initRx, initRy, initRz, initVx, initVy, initVz, initRdotV,
       initVdotV, initEner;
@@ -144,6 +134,12 @@ public:
   // flag to make sure initialize only once
   static bool flag_init;
 
+  double z = 0.5;
+  double blurb, zeta, tQ2;
+  int iSplit, pid_a, pid_b;
+  unsigned int max_color, min_color, min_anti_color;
+  double velocity[4], xStart[4], velocity_jet[4];
+  bool photon_brem = false;
 
   //qhat related functions
   int QhatParametrizationType;
@@ -179,6 +175,7 @@ public:
   void collHQ22(int CT, double temp, double qhat0ud, double v0[4], double p0[4],
                 double p2[4], double p3[4], double p4[4], double &qt);
 
+ElasticCollision elasticCollision;
 protected:
   uniform_real_distribution<double> ZeroOneDistribution;
 
@@ -188,4 +185,4 @@ private:
   double tscale;
 };
 
-#endif // MATTER_H
+#endif // Matter\_H
